224b67a72885c7c7da726d703add22e692bcfb03
alenka <alenka134@gmail.com>
2026-04-15 18:02:31 +0200

    Post title: h2 color #080d28; normalize math-bold Unicode on save

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,22 @@ from django.db import models
 from django.utils import timezone
 
 
+def _normalize_title_for_display(text):
+    """Map Unicode Mathematical Bold Latin (common ‘fancy’ titles) to plain ASCII so webfonts apply."""
+    if not text:
+        return text
+    out = []
+    for ch in text:
+        o = ord(ch)
+        if 0x1D400 <= o <= 0x1D419:
+            out.append(chr(ord('A') + (o - 0x1D400)))
+        elif 0x1D41A <= o <= 0x1D433:
+            out.append(chr(ord('a') + (o - 0x1D41A)))
+        else:
+            out.append(ch)
+    return ''.join(out)
+
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -26,6 +42,10 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def save(self, *args, **kwargs):
+        self.title = _normalize_title_for_display(self.title)
+        super().save(*args, **kwargs)
 
     @property
     def gallery_images(self):
