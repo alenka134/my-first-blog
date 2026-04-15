f6044ec1b3dd68a1128aff3a16cbafe984302eff
alenka <alenka134@gmail.com>
2026-04-15 14:24:15 +0200

    Pagination (3 per page), header-matched pagination styles, video2 order

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+POSTS_PER_PAGE = 3
+
+
 def post_list(request):
-    posts = Post.objects.filter(
+    qs = Post.objects.filter(
         published_date__lte=timezone.now()
     ).order_by('-published_date')
-
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(qs, POSTS_PER_PAGE)
+    page_obj = paginator.get_page(request.GET.get('page'))
+    elided_page_range = list(
+        paginator.get_elided_page_range(
+            page_obj.number,
+            on_each_side=1,
+            on_ends=1,
+        )
+    )
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'page_obj': page_obj,
+            'elided_page_range': elided_page_range,
+        },
+    )
